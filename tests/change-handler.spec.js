b117de07b74b208836f9c9716cf14810cf9edb81
brameijm <brameijm@gmail.com>
2019-03-11 18:20:02 -0400

    second commit

--- tests/change-handler.spec.js
+++ tests/change-handler.spec.js
@@ -17,11 +17,15 @@ describe("tests for change-handler", function() {
         // Remember, you can arrange, act, and assert...start small
         //arrange
         let changeHandler = new ChangeHandler(20,0);
-        // Act and Assert
-        expect(changeHandler.isPaymentSufficient('1')).toBe(false);
-        expect(changeHandler.isPaymentSufficient('20')).toBe(true);
-        expect(changeHandler.isPaymentSufficient('21')).toBe(true);
-
+        // Act and Assert an example of not enough cash
+        expect(changeHandler.isPaymentSufficient()).toBe(false);
+        changeHandler = new ChangeHandler(20,20);
+        // Act and Assert an example of just enough
+        expect(changeHandler.isPaymentSufficient()).toBe(true);
+        changeHandler = new ChangeHandler(20,21);
+        // Act and Assert an example of more then enough
+        expect(changeHandler.isPaymentSufficient()).toBe(true);
+        
     });
 
     it("tests the return change function", function() {
